35780ce9e0a12b818313a068528794db098dec13
Paul Harter <pl.hrtr@gmail.com>
2026-05-14 12:23:30 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Flake for STM32 C development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+              }
+              {
+                packages = with pkgs; [
+                  hterm
+                  cmake
+                  stm32cubemx
+                  gcc
+                  clang-tools
+                  gcc-arm-embedded
+                  ninja
+                  openocd
+                  stlink
+                ];
+
+                shellHook = "";
+              };
+        }
+      );
+
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
